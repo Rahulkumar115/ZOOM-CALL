b27e4f6241ab2d6fc71c7d47e6425cacb135bb61
Rahul Kumar <krahulr111@gmail.com>
2025-09-30 17:30:07 +0530

    Created routing for login and register

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,22 +1,31 @@
+import dotenv from 'dotenv';
+dotenv.config();
 import express from "express";
 import { createServer } from "node:http";
-
-import { Server } from "socket.io";
-
 import mongoose from "mongoose";
+import connectToSocket from './controllers/socketManager.js';
 
 import cors from "cors";
+import userRoutes from "./routes/users.routes.js";
 
 const app = express();
+const server = createServer(app);
+const io = connectToSocket(server);
+const url = process.env.MONGO_URL;
 
-app.get("/home", (req,res) => {
-    return res.json({"Hello": "world"});
-});
+app.set("port", (process.env.PORT || 8000));
+app.use(cors());
+app.use(express.json({limit: "40kb"}));
+app.use(express.urlencoded({limit: "40kb", extended: true}));
 
-const start = async() => {
+app.use("/api/v1/users", userRoutes);
 
-    app.listen(8000, () => {
-        console.log("app started");
+const start = async () => {
+    const connectionDb = await mongoose.connect(url);
+
+    console.log(`MONGO Connected DB Host: ${connectionDb.connection.host}`);
+    server.listen(app.get("port"), () => {
+        console.log("LISTENING ON PORT 8000");
     });
 };
 
