945c409f5bd175cf37a50f3c4c87e5db7faa82ba
Rahul Kumar <krahulr111@gmail.com>
2025-10-03 12:20:48 +0530

    Added files in frontend

--- frontend/src/pages/landing.jsx
+++ frontend/src/pages/landing.jsx
@@ -1,39 +1,53 @@
-import React from 'react';
-import "../App.css";
-import { Link } from 'react-router-dom';
-
+import React from 'react'
+import "../App.css"
+import { Link, useNavigate } from 'react-router-dom'
 export default function LandingPage() {
+
+
+    const router = useNavigate();
+
     return (
         <div className='landingPageContainer'>
             <nav>
-               <div className='navHeader'>
-                <h2>CALL U</h2>
+                <div className='navHeader'>
+                    <h2>CALL U</h2>
                 </div>
-               <div className='navlist'>
-                    <p>Join as Guest</p>
-                    <p>Register</p>
-                    <div role='button'>
-                       <p>Login</p> 
+                <div className='navlist'>
+                    <p onClick={() => {
+                        router("/aljk23")
+                    }}>Join as Guest</p>
+                    <p onClick={() => {
+                        router("/auth")
+
+                    }}>Register</p>
+                    <div onClick={() => {
+                        router("/auth")
+
+                    }} role='button'>
+                        <p>Login</p>
                     </div>
-                </div> 
+                </div>
             </nav>
 
 
-
-            <div className='landingMainContainer'>
+            <div className="landingMainContainer">
                 <div>
-                    <h1><span style={{color:"#FF9839"}}>Connect</span> with your loved Ones</h1>
+                    <h1><span style={{ color: "#FF9839" }}>Connect</span> with your loved Ones</h1>
 
-                    <p>Cover a distance by CALL U </p>
+                    <p>Cover a distance by CALL U</p>
                     <div role='button'>
                         <Link to={"/auth"}>Get Started</Link>
                     </div>
                 </div>
                 <div>
 
-                    <img src='/mobile.png' alt=''></img>
+                    <img src="/mobile.png" alt="" />
+
                 </div>
             </div>
+
+
+
         </div>
     )
 }
